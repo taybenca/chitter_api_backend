2dee527fa52ed32ef30c3676b3888d5b52d6b1a1
Kay Lovelace <kay@makersacademy.com>
2018-06-23 13:00:32 +0100

    Delete some merely marginally useful tests

--- spec/routing/sessions_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe SessionsController, type: :routing do
-  describe "routing" do
-    it "routes to #create" do
-      expect(:post => "/sessions").to route_to("sessions#create")
-    end
-  end
-end
