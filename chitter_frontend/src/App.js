2aff6661d6df75da8aae75cf841a691fc0068757
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-08-28 09:47:18 +0100

    add banner in the main page

--- chitter_frontend/src/App.js
+++ chitter_frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
+import Banner from './components/banner';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://chitter-backend-api-v2.herokuapp.com/peeps')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Banner />
+      {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.id}>
+               <p className="post-body">{post.body}</p>
+               <p className="post-date-created">{post.created_at}</p>
+               {/* <p className="post-date-updated">{post.updated_at}</p> */}
+               {/* <p className="post-user">{post.user.handle}</p> */}
+               {/* <p className="post-likes">{post.likes}</p> */}
+            </div>
+         );
+      })}
+   </div>
+   );
 }
+
 
 export default App;
---
+++ chitter_frontend/src/components/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+           <img src="banner.png" alt="banner"/> 
+        </header>
+    )
+}
+
+export default Banner
